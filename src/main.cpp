0005b4f460840808ef2cdc841bfe8e992562f948
aa1rey <bigfinger.bf@gmail.com>
2022-03-17 02:12:08 +0300

    modified src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-usingnamespace std;
-int main (nt argc, char* argv[]) {
+using namespace std;
+int main (int argc, char* argv[])
+{
 	cout << "Hello, world!" << endl;
 	return 0;
 }
